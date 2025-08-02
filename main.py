1d8dcacff0d52e9d458107e34b6015400340b189
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-08-01 19:12:30 -0600

    Opción 1

--- main.py
+++ main.py
@@ -21,7 +21,32 @@ while True:
     select = input("Seleccione una opción: ")
     match select:
         case 1:
-            pass
+            while True:
+                try:
+                    id = int(input("\nIngrese la ID del estudiante (5 dígitos): "))
+                    name = input("Ingrese el nombre del estudiante: ")
+                    carrera = input("Ingrese la carrera del estudiante: ")
+                    cursos = {}
+                    id_ok = True
+                    name_ok = True
+                    if id <=0 or len(str(id)) != 5:
+                        print("La ID debe ser positiva y de extactamente 5 dígitos")
+                        id_ok = False
+                    if not name.isalpha():
+                        print("El nombre no puede contener números")
+                        name_ok = False
+                    if id_ok and name_ok:
+                        break
+                except ValueError:
+                    print("Entrada de valores inválida, intente de nuevo")
+                except Exception as e:
+                    print("Error inesperado: ",e)
+            estudiantes[id] = {
+                "nombre": name,
+                "carrera": carrera,
+                "cursos": cursos
+            }
+
         case 2:
             pass
         case 3:
